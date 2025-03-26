5d54edfe56d03145a070949d22167a46dc3c3353
Eleazar-lopzm <eleazarlopez.jelm@ciencias.unam.mx>
2025-03-26 15:16:51 -0600

    Minipractica5 terminada?

--- Practica05/Practica05.ino
+++ Practica05/Practica05.ino
@@ -1,58 +1,64 @@
 /*
- *Universidad Nacional Autónoma de México
- *Facultad de Ciencias
- *Licenciatura en Ciencias de la Computación
- *Seminario de Ciencias de la Computación A: Introducción al Internet de las Cosas
- *
- *Escrito por: 
- *  Delgado Díaz Hermes Alberto
- *  López Montúfar José Eleazar
-*/
+  Universidad Nacional Autónoma de México
+  Facultad de Ciencias
+  Licenciatura en Ciencias de la Computación
+  Seminario de Ciencias de la Computación A: Introducción al Internet de las Cosas
 
+  Escrito por:
+    Delgado Díaz Hermes Alberto
+    López Montúfar José Eleazar
+*/
+// LED 3
 #define PinPWM 26
+// LED 4
 #define PinDAC 25
 #define PinADCPWM 13
 #define PinADCDAC 12
 
 //Variables para definir el PWM
-const int freq = 10000; //Frecuencia en Hz
-const int ChanelPWM = 0; //Canal 0 - 15
-const int resolution = 8; //Bits de resolución, hasta 8
-const float dacEscala = 7.0;
+const int freq = 5000; //Frecuencia en Hz
 
 void setup() {
   Serial.begin(115200);
+
+  // Configuración de pines
   pinMode(PinPWM, OUTPUT);
   pinMode(PinDAC, OUTPUT);
   pinMode(PinADCPWM, INPUT);
   pinMode(PinADCDAC, INPUT);
-  
-  ledcAttachChannel(PinPWM, freq, resolution, ChanelPWM); 
+  const int ChanelPWM = 0; //Canal 0 - 15
+  const int resolution = 8; //Bits de resolución, hasta 8
 
+  ledcAttachChannel(PinPWM, freq, resolution, ChanelPWM);
 }
 
 void loop() {
-  for(int i = 0; i<360; i++){
-    float seno = (sin(i * M_PI / 180)+1) * 127.5;
+  for (int i = 0; i < 360; i++) {
+    // Usar la misma función para ambos LEDs
+    float seno = sin(i * M_PI / 180);
+    float seno2 = (127*sin(i * M_PI / 180)+127);
+
+    // Escalar y centrar la señal para PWM (0-255)
+    int valorPWM = seno*125.4;
+
+    // Escalar y centrar la señal para DAC (0-255)
+    int valorDAC = seno2;
     
-    //round se usa para redondear un float de manera más precisa.
-    int valor = round(seno);
-    int valorDAC = round(seno * dacEscala);
-    
-    //constrain se usa para limitar un valor dentro de un rango específico.
-    valorDAC = constrain(valorDAC,0,255);
-    
+    ledcWrite(PinPWM, valorPWM);
     dacWrite(PinDAC, valorDAC);
-    ledcWrite(PinPWM, valor);
 
-    int adcDAC = analogRead(PinADCDAC);
-    int adcPWM = analogRead(PinADCPWM);
+    // Lectura de ADC y conversión a voltaje
+    float adcDAC = analogRead(PinADCDAC);
+    float adcPWM = analogRead(PinADCPWM);
+
+    float voltagedac = analogRead(PinADCDAC);
+    float voltagepwm = 3.3/4096 * analogRead(PinADCPWM);
     
-    Serial.print("PWM:");
-    Serial.print(adcPWM);
-    Serial.print(", DAC:");
-    Serial.println(adcDAC);
-
-    delay(10);
-    }
-} 
+    Serial.print("DAC: ");
+    Serial.print(voltagedac);
+    Serial.print(", PWM: ");
+    Serial.println(voltagepwm);
+    
+    delayMicroseconds(5000);
+  }
+}
